da00213fd76d700e366a80279151276de48532c7
Sara <sara.alvarezv@gmail.com>
2020-10-25 21:59:10 +0100

    Added main functionality: - Calibrate for 5 seconds - Measure from analog input - Clean up input by averaging 10 samples - Send to serial - Set led to value

--- measure_bend.ino
+++ measure_bend.ino
@@ -1,1 +1,95 @@
-//This is a test
+
+//  Based on Arduino smoothing, analog read and calibrate examples
+// http://www.arduino.cc/en/Tutorial/Smoothing
+
+// These constants won't change:
+const int sensorPin = A2;    // pin that the sensor is attached to
+const int ledPin = 13;        // pin that the LED is attached to
+
+// variables:
+int sensorValue = 0;         // the sensor value
+int sensorMin = 1023;        // minimum sensor value
+int sensorMax = 0;           // maximum sensor value
+
+
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+
+void setup() {
+
+  // initialize serial communication with computer:
+  Serial.begin(9600);
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
+  // turn on LED to signal the start of the calibration period:
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // calibrate during the first five seconds
+  while (millis() < 5000) {
+    sensorValue = analogRead(sensorPin);
+
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+    }
+  }
+
+  // signal the end of the calibration period
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+
+  // read the sensor:
+  sensorValue = analogRead(sensorPin);
+
+  // apply the calibration to the sensor reading
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
+
+  // in case the sensor value is outside the range seen during calibration
+  sensorValue = constrain(sensorValue, 0, 255);
+  readings[readIndex] = sensorValue;
+
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(1);        // delay in between reads for stability
+
+  // Serial.println(sensorValue);
+  // delay(200);
+
+  // fade the LED using the calibrated value:
+  analogWrite(13, average);
+  // fade the LED using the calibrated value:
+  // analogWrite(13, sensorValue);
+
+}
